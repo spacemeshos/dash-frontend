08150484522fa3597b42478700e1971e3934d334
Alex <motzart66@gmail.com>
2020-06-10 12:23:46 +0300

    add custom marker to map

--- src/components/molecules/Map/index.js
+++ src/components/molecules/Map/index.js
@@ -42,26 +42,11 @@ const Map = (props: Props) => {
                 />
               ))}
           </Geographies>
-          {markers.map(({ name, coordinates, markerOffset }) => (
+          {markers.map(({ name, coordinates }) => (
             <Marker key={name} coordinates={coordinates}>
-              <g
-                fill="none"
-                stroke="#FF5533"
-                strokeWidth="2"
-                strokeLinecap="round"
-                strokeLinejoin="round"
-                transform="translate(-12, -24)"
-              >
-                <circle cx="12" cy="10" r="3" />
-                <path d="M12 21.7C17.3 17 20 13 20 10a8 8 0 1 0-16 0c0 3 2.7 6.9 8 11.7z" />
-              </g>
-              <text
-                textAnchor="middle"
-                y={markerOffset}
-                style={{ fontFamily: 'system-ui', fill: '#5D5A6D' }}
-              >
-                {name}
-              </text>
+              <svg width="12" height="12" viewBox="0 0 12 12" fill="none" xmlns="http://www.w3.org/2000/svg">
+                <circle cx="6" cy="6" r="6" fill="#65B042" />
+              </svg>
             </Marker>
           ))}
         </ZoomableGroup>
