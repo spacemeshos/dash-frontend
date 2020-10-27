38210e1413cc99ee36d35dc4032d01053239933c
Alex <motzart66@gmail.com>
2020-10-27 10:10:18 +0200

    Update time

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -83,7 +83,7 @@ const Home = (props: Props) => {
         const incomeData = JSON.parse(message.data);
         setData(incomeData);
         // TODO 24 it's simple num, should be getting from backend
-        if (incomeData.lastapprovedlayer < incomeData.lastlayer + 24) {
+        if ((incomeData.lastlayer + 24) < incomeData.lastapprovedlayer) {
           uiStore.setNetworkStatus(ERROR_STATUS);
         } else if (incomeData.lastlayerts < ((Math.floor(Date.now() / 1000)) - (incomeData.layerduration))) {
           uiStore.setNetworkStatus(SYNCING_STATUS);
