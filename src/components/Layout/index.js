4274756536bd01566556cea2af99440ed72ebcc9
Alex <motzart66@gmail.com>
2020-11-04 14:55:32 +0200

    add url params

--- src/components/Layout/index.js
+++ src/components/Layout/index.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import Header from './Header';
 import darkLine from '../../assets/right_decoration.svg';
 import whiteLine from '../../assets/darkTheme/right_decoration_dark.svg';
@@ -9,15 +9,25 @@ const Layout = (props) => {
   const { children, viewStore } = props;
   const [checkedTheme, setCheckedTheme] = useState(localStorage.getItem('theme-color') || 'light');
 
-  const switchTheme = (e) => {
-    const color = e.target.checked ? 'dark' : 'light';
+  const setColor = (color: string) => {
     setCheckedTheme(color);
     document.documentElement.className = '';
     document.documentElement.classList.add(`theme-${color}`);
     localStorage.setItem('theme-color', color);
   };
 
+  const switchTheme = (e) => {
+    const color = e.target.checked ? 'dark' : 'light';
+    setColor(color);
+  };
+
   const hideLine = window.location.search.indexOf('hide-right-line') === -1;
+  const darkMode = window.location.search.indexOf('darkMode') !== -1;
+
+  useEffect(() => {
+    const color = darkMode ? 'dark' : 'light';
+    setColor(color);
+  }, [darkMode]);
 
   return (
     <LayoutContext.Provider value={{ checkedTheme }}>
