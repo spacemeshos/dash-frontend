cae9199fe73ce4e01918e1e6b36a5053017f4188
Alex <motzart66@gmail.com>
2020-12-16 15:52:12 +0200

    fix

--- src/helpers/mapping.js
+++ src/helpers/mapping.js
@@ -1,7 +1,1 @@
-export const reMappingNetworkArray = (data) => data.map((network) => {
-  return {
-    value: network.dashAPI,
-    label: network.netName,
-    explorer: network.explorer,
-  };
-});
+export const reMappingNetworkArray = (data) => data.map((network) => ({ value: network.dashAPI, label: network.netName, explorer: network.explorer }));
